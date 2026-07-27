216b1a5995d4fa36ee2f6b3556f7c9ef4f83e00f
Victor Rafael Rezende Celestino <vrcelestino@unb.br>
2026-07-27 20:15:07 +0200

    update files and results

--- src/gnn/__init__.py
+++ src/gnn/__init__.py
@@ -1,11 +1,7 @@
 from .models import GasseGNN, GasseBipartiteConv, LiangBiGNN
-from .ml_scheme_v2 import train_model, test_torch, test_sklearn
 
 __all__ = [
     "GasseGNN",
     "GasseBipartiteConv",
     "LiangBiGNN",
-    "train_model",
-    "test_torch",
-    "test_sklearn",
 ]
